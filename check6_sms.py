14db7272ea09540d41fe7f97c797783841a8dec0
aviot <aviotster@gmail.com>
2014-11-13 12:27:20 +0800

    Update check6_sms.py

--- check6_sms.py
+++ check6_sms.py
@@ -4,17 +4,17 @@ import time
 from twilio.rest import TwilioRestClient
 
 mail_send = 0
-ACCOUNT_SID = "ACc7cd037dfbd81fd6704c4db0dc7b6a5c"
-AUTH_TOKEN = "43ae2b0ac6c817c6e2b36c000ebae524"
+ACCOUNT_SID = [ACCOUNT_SID]
+AUTH_TOKEN = [AUTH_TOKEN]
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-while mail_send < 3:
+while mail_send < 10:
 
     response = urllib2.urlopen('https://play.google.com/store/devices/details?id=nexus_6_blue_32gb')
     a = response.read()
-    if 'We are out of inventory.' not in a or '<span class="play-button devices disabled">$649.00</span>' not in a :
+    if 'We are out of inventory.' not in a and '<span class="play-button devices disabled">$649.00</span>' not in a :
         client.messages.create(body="Nexus 6!!!!!!!!!!!!!!!",
-                    to="+8613128962965",
-                    from_="+13306178660",
+                    to="+country_code+phone",
+                    from_="+twilio_number",
                     )
         mail_send += 1
         time.sleep(60)
